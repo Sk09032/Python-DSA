80dd8d53faa1dc7f7fb1c4e4d4e8b0e6b5a65b7f
Sk09032 <kgpian121@gmail.com>
2026-03-09 15:22:26 +0530

    ADDED

--- jump_game_2.py
+++ jump_game_2.py
@@ -1,23 +1,42 @@
-if __name__=="__main__":
-    arr=list(map(int,input("Enter number ").split()))
-    
-    dp={}
-    
-    def helper(id):
-        if id in dp:
-            return dp[id]
-        if id>=len(arr)-1:
-            return 0
-        ans=1000000
+class Solution:
+    def __init__(self,nums,target):
+        self.nums=nums
+        self.target=target
         
-        for i in range(id+1,min(len(arr),id+arr[id]+1)):
-            ans=min(ans,1+helper(i))
-            
-        dp[id]=ans
-        return dp[id]        
+        
+    def solver(self)->bool:
+        
+        
+        nums=self.nums
+        target=self.target
+        st,end=0,len(nums)-1
+        
+        
+        while st<=end:
+            mid=(st+end)//2 
+            if nums[mid]==target:
+                return True
+            if nums[mid]>=nums[st]:
+                if nums[st]<=target<nums[mid]:
+                    end=mid-1
+                else:
+                    st=mid+1
+            elif nums[mid]<=nums[end]:
+                if nums[mid]<target<=nums[end]:
+                    st=mid+1
+                else:
+                    end=mid-1
+            else:
+                st+=1
+                
     
-    if helper(0)==1000000:
-        print("No ans Exit")
-    else:
-        print(helper(0))
-    
+        return False
+def main():
+    s=[3,4,5,6,7,0,1,2]
+    target=2
+    solution=Solution(s,target)
+    result=solution.solver()
+    print(result)
+
+if __name__=="__main__":
+    main()
